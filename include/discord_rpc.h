e1e7e3fdc417211c0f6b99ac29eb1929fd4fc880
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-03-24 15:14:13 +0200

    Update discord_rpc.h

--- include/discord_rpc.h
+++ include/discord_rpc.h
@@ -51,10 +51,10 @@ typedef struct DiscordUser {
     const char* username;
     const char* discriminator;
     const char* avatar;
-    const char* globalName;
+    const char* global_name;
     bool bot;
     int flags;
-    int premium;
+    int premium_type;
 } DiscordUser;
 
 typedef struct DiscordEventHandlers {
